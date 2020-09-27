2f628169a743fadb674e6510b58104ae6ea21a74
abdulhakeem abdulrahmon <adeolakinga@gmail.com>
2020-09-27 01:36:55 +0100

    projest working on

---
+++ script.js
@@ -0,0 +1,11 @@
+const firstSecMsvg=document.querySelectorAll("#first-section_mobile-svg");
+window.addEventListener("resize", function (){
+    // console.log(window.innerWidth)
+    // if (window.innerWidth <= 357){
+    //     console.log("inner")
+    // }
+    for(var i=window.innerWidth; i=== 357; i++){
+        console.log("forLoop")
+    }
+}
+)
